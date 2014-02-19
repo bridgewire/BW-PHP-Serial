ae28b3e508e64442dfb9656e8fbd34e9986eea93
Christiana Johnson <anajilly@gmail.com>
2014-02-19 03:44:28 -0800

    Make example.php compatible with changes and exhibit new features.

--- example.php
+++ example.php
@@ -4,25 +4,33 @@ include "php_serial.class.php";
 // Let's start the class
 $serial = new phpSerial;
 
-// First we must specify the device. This works on both linux and windows (if
-// your linux serial device is /dev/ttyS0 for COM1, etc)
-$serial->deviceSet("COM1");
-
-// We can change the baud rate, parity, length, stop bits, flow control
-$serial->confBaudRate(2400);
-$serial->confParity("none");
-$serial->confCharacterLength(8);
-$serial->confStopBits(1);
-$serial->confFlowControl("none");
+// // First we must specify the device. This works on both linux and windows (if
+// // your linux serial device is /dev/ttyS0 for COM1, etc)
+// $serial->deviceSet("COM1");
+// 
+// // We can change the baud rate, parity, length, stop bits, flow control
+// $serial->confBaudRate(2400);
+// $serial->confParity("none");
+// $serial->confCharacterLength(8);
+// $serial->confStopBits(1);
+// $serial->confFlowControl("none");
+// 
+// or do either of these instead with the same result:
+// $serial->confPort( "COM1", 2400, 8, 'none', 1, 'none' );
+// $serial->confPort( "COM1", 2400 );
+$serial->confPort( "COM1", 2400, 8, 'none', 1, 'none' );
 
 // Then we need to open it
 $serial->deviceOpen();
 
 // To write into
-$serial->sendMessage("Hello !");
+$serial->sendMessage("Hello !\r\n");
 
 // Or to read from
-$read = $serial->readPort();
+$read="";
+$read_length = $serial->readPort( $read );
+if( $read_length !== false && $read_length > 0 )
+    print "received: $read\n";
 
 // If you want to change the configuration, the device must be closed
 $serial->deviceClose();
