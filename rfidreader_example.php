e1feaac68ecb7a9d7b725963f29865563a8a6cb3
Christiana Johnson <anajilly@gmail.com>
2014-02-20 04:43:11 -0800

    change indent style to match php_serial.class.php. update vim mode-line.

--- rfidreader_example.php
+++ rfidreader_example.php
@@ -22,73 +22,73 @@ class RFID_BadKey_Exception extends Exception {};
 
 class rfid_key
 {
- /*
-  * This class defines an interface to RFID cards which are read by devices 
-  * such as ID-2LA, ID-12LA, and ID-20LA whose data is encoded according to
-  * the ASCII Data Format given in the datasheet which may be found here:
-  * http://dlnmh9ip6v2uc.cloudfront.net/datasheets/Sensors/ID/ID-2LA,%20ID-12LA,%20ID-20LA(2013-4-10).pdf
-  * or otherwise through google, or Sparkfun.
-  * 
-  * This  code is made to run on a Raspberry Pi whose UART has been "freed"
-  * and put into the service of the RFID reader named above. The details of
-  * the serial port configuation can be found in the datasheet also.
-  * 
-  */
+	/*
+	 * This class defines an interface to RFID cards which are read by devices 
+	 * such as ID-2LA, ID-12LA, and ID-20LA whose data is encoded according to
+	 * the ASCII Data Format given in the datasheet which may be found here:
+	 * http://dlnmh9ip6v2uc.cloudfront.net/datasheets/Sensors/ID/ID-2LA,%20ID-12LA,%20ID-20LA(2013-4-10).pdf
+	 * or otherwise through google, or Sparkfun.
+	 * 
+	 * This  code is made to run on a Raspberry Pi whose UART has been "freed"
+	 * and put into the service of the RFID reader named above. The details of
+	 * the serial port configuation can be found in the datasheet also.
+	 * 
+	 */
 
-  protected $hexdigits = array();
-  protected $withdashes = '';
-  protected $rawkey = '';
+	protected $hexdigits = array();
+	protected $withdashes = '';
+	protected $rawkey = '';
 
-  public function __toString() { return $this->withdashes; }
-  public function get_key()    { return $this->withdashes; }
-  public function get_rawkey() { return $this->rawkey; }
+	public function __toString() { return $this->withdashes; }
+	public function get_key()    { return $this->withdashes; }
+	public function get_rawkey() { return $this->rawkey; }
 
-  public function __construct( $key_string )
-  {
-    $this->parse_key( $key_string ); // thows excpetions if parsing fails. when parse_key
-                                     // finishes rawkey, withdashes, and hexdigits[] are set
-  }
+	public function __construct( $key_string )
+	{
+		$this->parse_key( $key_string ); // thows excpetions if parsing fails. when parse_key
+																		 // finishes rawkey, withdashes, and hexdigits[] are set
+	}
 
-  protected function parse_key( $key )
-  {
-    if( strlen($key) >= 16 )
-    {
-      // if the input string "$key" contains more than one key, ignore all but the first.
-      $matches = array();
-      $pattern = '/\002(([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2}))\r\n\003/i';
-      if( preg_match($pattern, $key, $matches ) )
-      {
-        $this->rawkey = $matches[1];
+	protected function parse_key( $key )
+	{
+		if( strlen($key) >= 16 )
+		{
+			// if the input string "$key" contains more than one key, ignore all but the first.
+			$matches = array();
+			$pattern = '/\002(([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2}))\r\n\003/i';
+			if( preg_match($pattern, $key, $matches ) )
+			{
+				$this->rawkey = $matches[1];
 
-        $sum = 0;
-        $checksum = hexdec( $matches[7] );
-        $frmtkey = "";
-        for( $i = 2; $i < 7; $i++ )
-        {
-          $this->hexdigits[] = $matches[$i];
-          $sum ^= hexdec( $matches[$i] );
-          $frmtkey .= ($i > 2 ? '-' : '').$matches[$i] ;
-        }
-        $this->hexdigits[] = $matches[7];
-        $frmtkey .= '-'.$matches[7];
+				$sum = 0;
+				$checksum = hexdec( $matches[7] );
+				$frmtkey = "";
+				for( $i = 2; $i < 7; $i++ )
+				{
+					$this->hexdigits[] = $matches[$i];
+					$sum ^= hexdec( $matches[$i] );
+					$frmtkey .= ($i > 2 ? '-' : '').$matches[$i] ;
+				}
+				$this->hexdigits[] = $matches[7];
+				$frmtkey .= '-'.$matches[7];
 
-        if( $sum == $checksum )
-          $this->withdashes = $frmtkey;
-        else
-          throw new RFID_BadKey_Exception("bad checksum for received key: $frmtkey");
-      }
-      else
-        throw new RFID_BadKey_Exception("key doesn't pass basic regex checking. received key: $key");
-    }
-    else
-      throw new RFID_BadKey_Exception("complete 16 bytes expected. received key: $key");
-  }
+				if( $sum == $checksum )
+					$this->withdashes = $frmtkey;
+				else
+					throw new RFID_BadKey_Exception("bad checksum for received key: $frmtkey");
+			}
+			else
+				throw new RFID_BadKey_Exception("key doesn't pass basic regex checking. received key: $key");
+		}
+		else
+			throw new RFID_BadKey_Exception("complete 16 bytes expected. received key: $key");
+	}
 }
 
 
 function errorhandler ( $errno, $errstr, $errfile, $errline )
 {
-  throw new Exception( basename($errfile).'['.$errline.'] (errno:'.$errno.') -- '.$errstr);
+	throw new Exception( basename($errfile).'['.$errline.'] (errno:'.$errno.') -- '.$errstr);
 }
 
 set_error_handler( 'errorhandler' );
@@ -97,44 +97,44 @@ $s = new phpSerial();
 
 try {
 
-  if( ! $s->confPort( '/dev/ttyAMA0', 9600 )) throw new Exception("confPort() failed");
-  if( ! $s->deviceOpen() )                    throw new Exception("deviceOpen() failed");
-  if( ! $s->confBlocking( false ) )           throw new Exception("confBlocking() failed");
+	if( ! $s->confPort( '/dev/ttyAMA0', 9600 )) throw new Exception("confPort() failed");
+	if( ! $s->deviceOpen() )                    throw new Exception("deviceOpen() failed");
+	if( ! $s->confBlocking( false ) )           throw new Exception("confBlocking() failed");
 
 } catch( Exception $e ) { print "exception: $e\n"; die; }
 
 $print_after_read = false;
 $keystring="";
 do {
-  $r = array( $s->getFilehandle() );
-  $w = null;
-  $e = null;
+	$r = array( $s->getFilehandle() );
+	$w = null;
+	$e = null;
 
-  $ready_count = stream_select( $r, $w, $e, NULL );
+	$ready_count = stream_select( $r, $w, $e, NULL );
 
-  if( $ready_count !== false && $ready_count > 0 )
-  {
-    $keylen = $s->readPort( $keystring );
+	if( $ready_count !== false && $ready_count > 0 )
+	{
+		$keylen = $s->readPort( $keystring );
 
-    if( $print_after_read )  print "readPort() returned $keylen bytes\n";
+		if( $print_after_read )  print "readPort() returned $keylen bytes\n";
 
-    if( strlen( $keystring ) >= 16 )
-    {
-      try
-      {
-        $key = new rfid_key( $keystring );
-        print "received valid rfid key: $key\n";
-        $keystring=""; // consume the string after success
-      }
-      catch (Exception $e) { print "exception: $e\n"; }
+		if( strlen( $keystring ) >= 16 )
+		{
+			try
+			{
+				$key = new rfid_key( $keystring );
+				print "received valid rfid key: $key\n";
+				$keystring=""; // consume the string after success
+			}
+			catch (Exception $e) { print "exception: $e\n"; }
 
-      // just in case parsing the key fails repeated we truncate
-      // remove leading, useless data from time to time.
-      if( strlen( $keystring ) >= 32 )
-        $keystring = substr( $keystring, 16 );
-    }
-  }
+			// just in case parsing the key fails repeated we truncate
+			// remove leading, useless data from time to time.
+			if( strlen( $keystring ) >= 32 )
+				$keystring = substr( $keystring, 16 );
+		}
+	}
 } while( true );
 
-/* vim: set ai et tabstop=2  shiftwidth=2: */
+/* vim: set ai noet ts=4  sw=4: */
 ?>
